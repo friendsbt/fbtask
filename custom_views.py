f719578bd311ef2c4d0a9b26fefdc09b9357587c
bonelee <bone@bone.com>
2015-06-22 21:01:54 +0800

    use jsonp to support get_token

--- custom_views.py
+++ custom_views.py
@@ -1,5 +1,8 @@
+import json
 import django
 from django.http import HttpResponse
 
 def get_token(request):
-    return HttpResponse(django.middleware.csrf.get_token(request))
+    token = django.middleware.csrf.get_token(request)
+    data = '%s("%s");' % (request.REQUEST['callback'], token)
+    return HttpResponse(data, "text/javascript")
